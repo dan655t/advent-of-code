4b6ddfc8d696319dce29f6cd83ea6bc9dff00cb7
Daniel Graham <dan655t@gmail.com>
2021-12-03 12:32:23 -0500

    refactor day 3

--- 2021/03/Program.cs
+++ 2021/03/Program.cs
@@ -12,39 +12,28 @@ var mostCommons = lines
 
 var gamma = new string(mostCommons.SelectMany(x => x ? "1" : "0").ToArray());
 var epsilon = new string(mostCommons.SelectMany(x => x ? "0" : "1").ToArray());
-
 Console.WriteLine(Convert.ToInt32(gamma, 2) * Convert.ToInt32(epsilon, 2));
 
-var remainingLines = lines.ToList();
-for (var i = 0; i < wordLength; i++)
-{
-    var zeroes = remainingLines.Count(l => l[i] == '0');
-    var ones = remainingLines.Count(l => l[i] == '1');
-    var keepChar = zeroes > ones ? '0' : '1';
-    remainingLines = remainingLines
-        .Where(l => l[i] == keepChar)
-        .ToList();
-    if (remainingLines.Count == 1)
-    {
-        break;
-    }
-}
-var oxygenRating = Convert.ToInt32(remainingLines[0], 2);
-
-remainingLines = lines.ToList();
-for (var i = 0; i < wordLength; i++)
-{
-    var zeroes = remainingLines.Count(l => l[i] == '0');
-    var ones = remainingLines.Count(l => l[i] == '1');
-    var keepChar = zeroes > ones ? '1' : '0';
-    remainingLines = remainingLines
-        .Where(l => l[i] == keepChar)
-        .ToList();
-    if (remainingLines.Count == 1)
-    {
-        break;
-    }
-}
-var scrubberRating = Convert.ToInt32(remainingLines[0], 2);
-
+var oxygenRating = GetRating(lines, (zeroes, ones) => zeroes > ones ? '0' : '1');
+var scrubberRating = GetRating(lines, (zeroes, ones) => zeroes > ones ? '1' : '0');
 Console.WriteLine(oxygenRating * scrubberRating);
+
+static int GetRating(string[] lines, Func<int, int, char> keepCharFunc)
+{
+    var wordLength = lines[0].Length;
+    var remainingLines = lines.ToList();
+    for (var i = 0; i < wordLength; i++)
+    {
+        var zeroes = remainingLines.Count(l => l[i] == '0');
+        var ones = remainingLines.Count(l => l[i] == '1');
+        var keepChar = keepCharFunc(zeroes, ones);
+        remainingLines = remainingLines
+            .Where(l => l[i] == keepChar)
+            .ToList();
+        if (remainingLines.Count == 1)
+        {
+            break;
+        }
+    }
+    return Convert.ToInt32(remainingLines[0], 2);
+}
