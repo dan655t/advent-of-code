74042f406f0057e866d2e75bd1715e1ed3106e48
Daniel Graham <dan655t@gmail.com>
2021-12-07 09:26:58 -0500

    day 7

---
+++ 2021/07/Program.cs
@@ -0,0 +1,14 @@
+﻿var positions = File.ReadAllText("input.txt")
+    .Split(',')
+    .Select(int.Parse);
+
+var fuelCost1 = LowestFuelCost(positions, distance => distance);
+Console.WriteLine(fuelCost1);
+var fuelCost2 = LowestFuelCost(positions, distance => Enumerable.Range(1, distance).Aggregate(0, (acc, x) => acc += x));
+Console.WriteLine(fuelCost2);
+
+static int LowestFuelCost(IEnumerable<int> positions, Func<int, int> fuelCost)
+    => positions
+        .Select((pos) => positions.Select(dest => fuelCost(Math.Abs(pos - dest))).Sum())
+        .OrderBy(x => x)
+        .First();
